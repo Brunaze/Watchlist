86e445c0baf852a27b10f4bf21e8a4b89a2e211e
Brunaze <brunodspinto98@gmail.com>
2023-03-24 11:16:13 +0100

    UtilisateurRepository login

--- Back/watchlist/src/main/java/fr/solutec/watchlist/repository/UtilisateurRepository.java
+++ Back/watchlist/src/main/java/fr/solutec/watchlist/repository/UtilisateurRepository.java
@@ -2,9 +2,14 @@ package fr.solutec.watchlist.repository;
 
 import fr.solutec.watchlist.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
+    @Query("SELECT u FROM Utilisateur u WHERE u.username = ?1 and u.password = ?2")
+    public Optional<Utilisateur> findUtilisateurByUsernameAndPassword(String username, String password);
 }
